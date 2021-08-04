d2ee7b9b6b5148613fee97350d09087e22f83e02
João <hlabeats@gmail.com>
2021-08-03 22:46:20 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [alunes, setAlunes] = useState([
+    "Adriano Nascimento",
+    "Bruno",
+    "Carlos Eduardo",
+    "Elias Santana"
+
+  ]);
+
+
+  const [title, setTitle] = useState("Lista de chamada da turma 2A")
+
+  const [registro, setRegistro] = useState("");
+
+  useEffect(()=>{
+    localStorage.setItem("alunes", JSON.stringify(alunes))
+  },[alunes]);
+
+
+  useEffect(()=>{
+    const alunesReg = localStorage.getItem("alunes");
+
+    if (alunesReg){
+
+      setAlunes(JSON.parse(alunesReg));
+    }
+  }, []);
+
+  function adicionando(){
+    setAlunes([...alunes,registro]);
+    setRegistro("");
+  };
+
+  const checking = 
+  useState(
+  <div className="presente"><p>Presente?</p><input type="checkbox" ></input><hr></hr></div>);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+                <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://blueedtech.com.br"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Acesse a Blue
         </a>
       </header>
+      
+      <>
+    <div className="table">
+    <ul>
+      <h2>{title}</h2><hr></hr>
+    {alunes.map((alune)=>(<li key = {alune}>{alune}{checking}</li>))}
+    </ul>
+    </div>
+    <input className="entrada" type="text" value= {registro} onChange={(e)=> setRegistro(e.target.value)}
+    />
+    <button type="button" onClick={adicionando}>Add</button>
+    </>
     </div>
   );
 }
